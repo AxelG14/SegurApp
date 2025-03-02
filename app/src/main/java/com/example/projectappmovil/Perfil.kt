c9ce10e107593185744f656a48fe410e2168a26a
Axel <dereck1409@outlook.es>
2025-03-01 20:53:51 -0500

    Se creo la interfaz del perfil

---
+++ app/src/main/java/com/example/projectappmovil/Perfil.kt
@@ -0,0 +1,209 @@
+package com.example.projectappmovil
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.projectappmovil.navegation.AppScreens
+
+@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Perfil(){
+    Scaffold (
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("",
+                    fontSize = 20.sp
+                ) },
+                navigationIcon = {
+                    Image(
+                        painter = painterResource(R.drawable.vueloenavion),
+                        contentDescription = null,
+                        modifier = Modifier.size(50.dp)
+                    )
+                },
+                actions = {
+                    IconButton(
+                        onClick = { },
+                        colors = IconButtonDefaults.iconButtonColors(
+                            contentColor = Color.Black,
+                            containerColor = Color.White
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = null,
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.DarkGray)
+            )
+
+        },
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    onClick = {},
+                    selected = true,
+                    icon = { Icon(imageVector = Icons.Default.Home, contentDescription = null) },
+                    label = { Text("MENU") }
+                )
+                NavigationBarItem(
+                    onClick = {},
+                    selected = false,
+                    icon = { Icon(imageVector = Icons.Default.Place, contentDescription = null) },
+                    label = { Text("REPORTS") }
+                )
+                NavigationBarItem(
+                    onClick = {},
+                    selected = false,
+                    icon = { Icon(imageVector = Icons.Default.Create, contentDescription = null) },
+                    label = { Text("MINE") }
+                )
+                NavigationBarItem(
+                    onClick = {},
+                    selected = false,
+                    icon = { Icon(imageVector = Icons.Default.Notifications, contentDescription = null) },
+                    label = { Text("NOTIFI") }
+                )
+            }
+
+        }
+
+    ) { innerPadding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp)
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "Perfil",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            var nombre by remember { mutableStateOf("") }
+            var ciudad by remember { mutableStateOf("") }
+            var direccion by remember { mutableStateOf("") }
+            var email by remember { mutableStateOf("") }
+            var contraseña by remember { mutableStateOf("") }
+            TextField(
+                value = nombre,
+                onValueChange = { newText -> nombre = newText },
+                label = { Text("Nombre") },
+                modifier = Modifier.fillMaxWidth(0.8f),
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Person, contentDescription = null)
+                }
+            )
+            TextField(
+                value = ciudad,
+                onValueChange = { newText -> ciudad = newText },
+                label = { Text("Ciudad") },
+                modifier = Modifier.fillMaxWidth(0.8f),
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Home, contentDescription = null)
+                }
+            )
+            TextField(
+                value = direccion,
+                onValueChange = { newText -> direccion = newText },
+                label = { Text("Dirección") },
+                modifier = Modifier.fillMaxWidth(0.8f),
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Place, contentDescription = null)
+                }
+            )
+            TextField(
+                value = email,
+                onValueChange = { newText -> email = newText },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth(0.8f),
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Email, contentDescription = null)
+                }
+            )
+            TextField(
+                value = contraseña,
+                onValueChange = { newText -> contraseña = newText },
+                label = { Text("Contraseña") },
+                modifier = Modifier.fillMaxWidth(0.8f),
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Create, contentDescription = null)
+                }
+            )
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth(0.7f)
+            ) {
+                Text(text = "GUARDAR")
+            }
+
+                Button(
+                    onClick = { },
+                    modifier = Modifier.fillMaxWidth(0.5f),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.secondary,
+                    )
+                ) {
+                    Text(text = "CERRAR SESION")
+
+                }
+                Button(
+                    onClick = { },
+                    modifier = Modifier.fillMaxWidth(0.5f),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Red,
+                    )
+                ) {
+                    Text(text = "ELIMINAR CUENTA")
+
+                }
+
+        }
+    }
+}
--- app/src/main/java/com/example/projectappmovil/Reports.kt
+++ app/src/main/java/com/example/projectappmovil/Reports.kt
@@ -1,23 +1,13 @@
 
 package com.example.projectappmovil
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Home
@@ -37,33 +27,23 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
-import com.example.projectappmovil.navegation.AppNavigation
-import com.example.projectappmovil.ui.theme.ProjectAppMovilTheme
-import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.inappmessaging.model.ImageData
 import com.google.firebase.ktx.Firebase
 
 @OptIn(ExperimentalMaterial3Api::class)
-
-
 @Composable
 fun Reports1() {
     val auth: FirebaseAuth = Firebase.auth
@@ -136,22 +116,6 @@ fun Reports1() {
     ) { innerPadding ->
             LoadImageFromFirestore(userId!!, innerPadding)
     }
-
-//            item {
-//                Card(
-//                    modifier = Modifier
-//                        .size(height = 300.dp, width = 350.dp)
-//
-//                ) {
-//                    Image(
-//                        painter = rememberAsyncImagePainter(images[0].url),
-//                        contentDescription = null,
-//                        modifier = Modifier.fillMaxSize()
-//                    )
-//
-//                    Text(text = images[0].title)
-//                }
-//            }
 }
 
 @Composable
@@ -162,7 +126,6 @@ fun LoadImageFromFirestore(userId: String, innerPadding: PaddingValues) {
     var description by remember { mutableStateOf("") }
     var ubication by remember { mutableStateOf("") }
 
-    // Recuperar los datos de Firestore
     LaunchedEffect(Unit) {
         db.collection("clientes")
             .document(userId)
@@ -209,7 +172,6 @@ fun LoadImageFromFirestore(userId: String, innerPadding: PaddingValues) {
                     Text(text = "Ubicacion: $ubication" ,
                         modifier = Modifier.padding(horizontal = 16.dp)
                     )
-
                 }
             }
         }
